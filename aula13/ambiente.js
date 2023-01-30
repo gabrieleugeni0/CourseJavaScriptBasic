62c8d748c0c6d2a55a1c2eb8cd46792401253d6e
Gabriel Rodrigues <gabrieleugenio90@gmail.com>
2023-01-30 11:33:41 -0300

    aulas 13 e 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,23 @@
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`tudo bem? passo ${c}`)
+    c ++ // c += 1 == c = c + 1
+}
+*/
+
+var c = 1
+do {
+    console.log(`tudo bem? passo ${c}`)
+    c ++
+} while (c <= 6)
+
+
+/*
+console.log('tudo bem? passo 1')
+console.log('tudo bem? passo 2')
+console.log('tudo bem? passo 3')
+console.log('tudo bem? passo 4')
+console.log('tudo bem? passo 5')
+console.log('tudo bem? passo 6')
+*/
---
+++ aula14/ambiente.js
@@ -0,0 +1,21 @@
+console.log('Vai comecar...')
+for (var c = 1; c <= 6; c++) {
+    console.log(c)
+}
+console.log('Fim!')
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`tudo bem? passo ${c}`)
+    c ++ // c += 1 == c = c + 1
+}
+*/
+
+/*
+var c = 1
+do {
+    console.log(`tudo bem? passo ${c}`)
+    c ++
+} while (c <= 6)
+*/

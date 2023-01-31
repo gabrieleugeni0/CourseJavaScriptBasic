49567a296bf16f6cbaa19a3360a88807edbacc21
Gabriel Rodrigues <gabrieleugenio90@gmail.com>
2023-01-31 11:01:59 -0300

    exercicio 016 finalizado

--- ex16e17/ex016/script.js
+++ ex16e17/ex016/script.js
@@ -1,25 +1,42 @@
 function contador() {
-    var inicio = document.querySelector('input#inicio')
-    var fim = document.querySelector('input#fim')
-    var passo = document.querySelector('input#passo')
-    var res = document.querySelector('div#res')
+    let inicio = document.querySelector('input#inicio')
+    let fim = document.querySelector('input#fim')
+    let passo = document.querySelector('input#passo')
+    let restxt = document.querySelector('p#restxt')
+    let res = document.querySelector('p#res')
+    res.innerHTML = ''
     if (inicio.value.length == 0 || fim.value.length == 0) {
+        restxt.innerHTML = '[ERRO]'
         res.innerHTML = 'Impossivel contar! Preencha os campos corretamente!'
+    } else if (Number(fim.value) == Number(inicio.value)) {
+        restxt.innerHTML = '[ERRO]'
+        res.innerHTML = 'Impossivel contar! Inicio e o mesmo do Fim!'
+    } else if (Number(passo.value) < 0) {
+        restxt.innerHTML = '[ERRO]'
+        res.innerHTML = 'Impossivel contar! Passo negativo (considerar passo positivo para countdowns)!'
     } else {
-        if (Number(fim.value) == Number(inicio.value)) {
-            res.innerHTML = 'Impossivel contar! Inicio e o mesmo do Fim!'
-        } else {
-            if (passo.value.length == 0 || Number(passo.value) == 0) {
-                window.alert('Passo nulo ou nao preenchido! Contando de 1 em 1!')
-                passo = 1
-            }    
-            if (Number(inicio.value) > Number(fim.value)) {
-                window.alert('Contagem decresciva!')
-                if (Number(passo.value) > 0) {
-                    passo = -passo
-                }
-            }
+        if (passo.value.length == 0 || passo.value == '0') {
+            passo.value = '1'
+            window.alert('Passo nao digitado ou nulo. Passo considerado: 1')
         }
+        restxt.innerHTML = 'Contando:'
+        if (Number.parseInt(inicio.value) > Number.parseInt(fim.value)) {
+            for (let c = Number.parseInt(inicio.value); c >= Number.parseInt(fim.value); c -= Number.parseInt(passo.value)) {
+                if (c == Number.parseInt(inicio.value)) {
+                    res.innerHTML += `${c} `             
+                } else {
+                    res.innerHTML += ` &#128073 ${c}`
+                }              
+            }
+        } else {
+            for (let c = Number.parseInt(inicio.value); c <= Number.parseInt(fim.value); c += Number.parseInt(passo.value)) {
+                if (c == Number.parseInt(inicio.value)) {
+                    res.innerHTML += `${c} `             
+                } else {
+                    res.innerHTML += ` &#128073 ${c}`
+                }              
+            }
+        }       
+        res.innerHTML += ` &#128073 &#x1F6A9`
     }
-    res.innerHTML = 'FIM'
 }

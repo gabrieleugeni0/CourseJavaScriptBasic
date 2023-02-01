f042b616b0bc75adf2580bf1c968673d644344c2
Gabriel Rodrigues <gabrieleugenio90@gmail.com>
2023-02-01 09:43:42 -0300

    refatoracao exercicio 16 e atualizacao readme

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 Arquivos separados em aula e exercícios
 
 #### Exercícios:
-- ex01: HTML page identificando horário atual, informando e dando as saudações. As imagens e cores se adaptam aos horários.
-- ex02: HTML page perguntando qual o ano de nascimento e qual o sexo. Idade é calculada e é mostrada uma imagem correspondente ao gênero e a faixa de idade do usuário.
+- ex014: HTML page identificando horário atual, informando e dando as saudações. As imagens e cores se adaptam aos horários.
+- ex015: HTML page perguntando qual o ano de nascimento e qual o sexo. Idade é calculada e é mostrada uma imagem correspondente ao gênero e a faixa de idade do usuário.
+- ex016: HTML page solicitando um inínio, um fim e um passo ao usuário. Ao pressionar o botão contar a contagem é exibida na tela conforme a necessidade do usuário. Pequenos tratamentos de possíveis entradas inválidas realizado.
--- ex16e17/ex016/script.js
+++ ex16e17/ex016/script.js
@@ -1,42 +1,27 @@
-function contador() {
-    let inicio = document.querySelector('input#inicio')
-    let fim = document.querySelector('input#fim')
-    let passo = document.querySelector('input#passo')
-    let restxt = document.querySelector('p#restxt')
-    let res = document.querySelector('p#res')
-    res.innerHTML = ''
-    if (inicio.value.length == 0 || fim.value.length == 0) {
-        restxt.innerHTML = '[ERRO]'
-        res.innerHTML = 'Impossivel contar! Preencha os campos corretamente!'
-    } else if (Number(fim.value) == Number(inicio.value)) {
-        restxt.innerHTML = '[ERRO]'
-        res.innerHTML = 'Impossivel contar! Inicio e o mesmo do Fim!'
-    } else if (Number(passo.value) < 0) {
-        restxt.innerHTML = '[ERRO]'
-        res.innerHTML = 'Impossivel contar! Passo negativo (considerar passo positivo para countdowns)!'
+function contar() {
+    let ini = document.getElementById('txtini')
+    let fim = document.getElementById('txtfim')
+    let passo = document.getElementById('txtpas')
+    let res = document.getElementById('res')
+    if (ini.value.length == 0 || fim.value.length == 0) {
+        res.innerHTML = '[ERRO] Dados nao preenchidos!'      
+    } else if (Number(ini.value) == Number(fim.value)) {
+        res.innerHTML = '[ERRO] Inicio igual a fim!'
     } else {
-        if (passo.value.length == 0 || passo.value == '0') {
+        if (Number(passo.value) <= 0) {
+            window.alert('Passo nao preenchido ou zero invalido! Passo 1 considerado.')
             passo.value = '1'
-            window.alert('Passo nao digitado ou nulo. Passo considerado: 1')
         }
-        restxt.innerHTML = 'Contando:'
-        if (Number.parseInt(inicio.value) > Number.parseInt(fim.value)) {
-            for (let c = Number.parseInt(inicio.value); c >= Number.parseInt(fim.value); c -= Number.parseInt(passo.value)) {
-                if (c == Number.parseInt(inicio.value)) {
-                    res.innerHTML += `${c} `             
-                } else {
-                    res.innerHTML += ` &#128073 ${c}`
-                }              
+        res.innerHTML = 'Contando: <br><br>'
+        if (Number(ini.value) < Number(fim.value)) {
+            for(let c = Number(ini.value); c <= Number(fim.value); c += Number(passo.value)) {
+                res.innerHTML += `${c} &#x1f449 `
             }
-        } else {
-            for (let c = Number.parseInt(inicio.value); c <= Number.parseInt(fim.value); c += Number.parseInt(passo.value)) {
-                if (c == Number.parseInt(inicio.value)) {
-                    res.innerHTML += `${c} `             
-                } else {
-                    res.innerHTML += ` &#128073 ${c}`
-                }              
+        } else if (Number(ini.value) > Number(fim.value)) {
+            for(let c = Number(ini.value); c >= Number(fim.value); c -= Number(passo.value)) {
+                res.innerHTML += `${c} &#x1f449 `
             }
-        }       
-        res.innerHTML += ` &#128073 &#x1F6A9`
+        }
+        res.innerHTML += `&#x1F6A9`
     }
 }

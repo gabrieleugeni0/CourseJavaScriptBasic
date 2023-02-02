f10576dd182c7069b1a13af61a25908bc8bcf6cf
Gabriel Rodrigues <gabrieleugenio90@gmail.com>
2023-02-02 10:29:17 -0300

    aula 15

---
+++ aula15/ambiente.js
@@ -0,0 +1,40 @@
+let num = ['x', 8, 2, 9, 3, 2.54, true, false, 'alo', 'teste1', 'teste2']
+
+// console.log(`Tipo da variavel: ${typeof(num)}\nConteudo: ${num}`)
+
+// num[5] = 7
+
+// console.log(num)
+
+// num.push(1)
+
+// console.log(num)
+
+// console.log(num.length)
+
+// num[num.length] = 'teste'
+
+// for (var c = 0; c <= (num.length - 1); c++) {
+//     console.log(num[c])
+// }
+
+let arr = ['a', 'y', 'z', 5, 8, 4, 'w', 3, 'x', 'teste'] 
+
+num.sort()
+// arr.sort()
+
+
+// console.log(`${num}\n`)
+// console.log(`${arr}`)
+
+// console.log(arr[0])
+
+// for (let i = 0; i <= (arr.length - 1); i++) {
+//     console.log(`A posicao ${i} tem o valor ${arr[i]}`)
+// }
+
+// for (let i in arr) {
+//     console.log(`A posicao ${i} tem o valor ${arr[i]}`)
+// }
+
+console.log(arr.indexOf('x'))
